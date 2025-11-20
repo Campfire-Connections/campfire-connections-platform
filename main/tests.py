0cd27e84e501a6dd5bebf39e23abacae741de1f6
David Durost <david.durost@gmail.com>
2025-11-19 19:45:20 -0500

    Add healthcheck view test to main app

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class MainViewTests(TestCase):
+    def test_healthcheck_returns_ok(self):
+        response = self.client.get(reverse("healthcheck"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"ok", response.content)
